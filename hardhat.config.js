9f60b92f035ce770f2766d279cb4fb7b6fcdf2c4
tonywtf <146009228+roadsidedev@users.noreply.github.com>
2024-08-29 07:16:50 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: [`0x2ba6ccb61e2e0a56a1688e978fb7e1c368c53b9fb87b6c1a4ef7d821fd1e0359`],
+      accounts: [`PRIVATE_KEYS`],
     },
   },
 };
