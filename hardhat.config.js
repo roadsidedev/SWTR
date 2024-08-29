00c4c8e4f95f509fea04338ccd322a156796f7f4
roadsidelab dev <roadsidelabinc@gmail.com>
2024-08-29 06:13:33 +0000

    minting NFT

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.20",
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: ["0x"], //Your private key starting with "0x"
+      accounts: [`0x2ba6ccb61e2e0a56a1688e978fb7e1c368c53b9fb87b6c1a4ef7d821fd1e0359`],
     },
   },
 };
